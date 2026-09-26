473d5c9042312609df046a8cc3158925edd4507d
steve3rab <130780043+steve3rab@users.noreply.github.com>
2026-09-26 11:29:22 +0200

    Add Creo wrapper: ArchiScopeGuard.hpp

---
+++ ArchiScopeGuard.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+namespace creo {
+
+    template<typename F>
+    class ArchiScopeGuard {
+      public:
+        explicit ArchiScopeGuard(F f) noexcept(std::is_nothrow_move_constructible_v<F>) :
+            f_(std::move(f)) {
+        }
+
+        ArchiScopeGuard(ArchiScopeGuard&& other) noexcept(std::is_nothrow_move_constructible_v<F>) :
+            f_(std::move(other.f_)),
+            active_(other.active_) {
+            other.active_ = false;
+        }
+
+        ArchiScopeGuard(const ArchiScopeGuard&) = delete;
+        ArchiScopeGuard& operator=(const ArchiScopeGuard&) = delete;
+        ArchiScopeGuard& operator=(ArchiScopeGuard&&) = delete;
+
+        ~ArchiScopeGuard() {
+            if (active_) {
+                f_();
+            }
+        }
+
+        void Dismiss() noexcept {
+            active_ = false;
+        }
+
+      private:
+        F f_;
+        bool active_ = true;
+    };
+
+    template<typename F>
+    ArchiScopeGuard<std::decay_t<F>> Defer(F&& f) {
+        return ArchiScopeGuard<std::decay_t<F>>(std::forward<F>(f));
+    }
+
+}
